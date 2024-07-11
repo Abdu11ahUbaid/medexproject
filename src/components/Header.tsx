37d228d66672cdbba2d04b622efe017a75531f6b
Abdu11ahUbaid <123170446+Abdu11ahUbaid@users.noreply.github.com>
2024-07-11 15:29:14 +0500

    Initialize project using Create React App

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Grid, Typography,  Box, Container , useMediaQuery, } from "@mui/material";
+
+export default function Header() {
+  return (
+    
+    <> 
+       <div> 
+        Header 
+              <Grid>hi</Grid>
+       </div>
+
+
+    </>
+  )
+}
